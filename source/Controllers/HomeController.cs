e4d0dcc3a7e0ff8fbe577a27d955930e7fd80e58
beyza çay <beyzacay@staj.com.tr>
2024-02-15 18:55:07 +0300

    change controller

---
+++ source/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DyslexiaEduGameApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
