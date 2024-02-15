aa297eaadc20031beb00fcab071ff750711aa16f
Mert Aydın <mmertaydin4@gmail.com>
2024-02-15 18:19:46 +0300

    Create Database

---
+++ source/Models/DyslexiaDiagnosis.cs
@@ -0,0 +1,15 @@
+﻿using DyslexiaEduGameApp.Models.Entities;
+
+namespace DyslexiaEduGameApp.Models
+{
+    public class DyslexiaDiagnosis : BaseEntity
+    {
+        public Guid UserId { get; set; }
+        public int TestResults { get; set; }
+        public string FeedBack { get; set; }
+        public string Description { get; set; }
+        public virtual ApplicationUser Users { get; set; }
+        public virtual ICollection<MatchingGame> MatchingGames { get; set; }
+        public virtual ICollection<NavigationGame> NavigationGames { get; set; }
+    }
+}
---
+++ source/Models/Support.cs
@@ -0,0 +1,15 @@
+﻿using DyslexiaEduGameApp.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace DyslexiaEduGameApp.Models
+{
+    public class Support : BaseEntity
+    {
+        public TimeSpan TimeSpent { get; set; }
+        public string FAQs { get; set; }
+        public string ContactString { get; set; }
+        public string Message { get; set; }
+        public string SupportStatus { get; set; }
+        public virtual ICollection<Sistem> Sistems { get; set; }
+    }
+}
