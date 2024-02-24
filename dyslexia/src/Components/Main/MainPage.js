175d3b8a0e7e38ac92a4113322bd034ea8196a02
pinarrcindemirr <pnrcndmr01@gmail.com>
2024-02-24 12:49:28 +0300

    Create Main Page

--- dyslexia/src/Components/Main/MainPage.js
+++ dyslexia/src/Components/Main/MainPage.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import './Main.css'
+import { ImStarFull } from "react-icons/im";
 
 const MainPage = () => {
   return (
-    <div>
-      Main Page
+    <div className='main-container'>
+      <div className='profile-icon'>
+        <ImStarFull className="icon" />
+        <span className="text">PROFILE</span>
+      </div>
+       <div className="card-container">
+        <div className="card">
+          Diagnosis Tests
+          {/* You can add more content here */}
+        </div>
+        <div className="card">
+          Educational Games
+          {/* You can add more content here */}
+        </div>
+      </div>
     </div>
   )
 }
 
 export default MainPage
+
