4ffb648fd587fecde16348b4867aa29aad0458a9
pinarrcindemirr <pnrcndmr01@gmail.com>
2024-02-25 21:45:27 +0300

    Diagnostic Test Pages created

--- dyslexia/src/Components/Main/DiagnosticTest/DiagnosticTestAgreement.js
+++ dyslexia/src/Components/Main/DiagnosticTest/DiagnosticTestAgreement.js
@@ -7,14 +7,23 @@ import { IoIosCloudy } from "react-icons/io";
 
 const DiagnosticTestAgreement = () => {
     const navigate = useNavigate();
+    const [agree, setAgree] = useState(false);
+    const [error, setError] = useState(false);
 
     const handleSave = () => {
-         navigate('/MainPage');
-      };
+        if (agree) {
+            navigate('/TestsInformation');
+          } else {
+            setError(true);
+          }
+    };
+    const handleClose = () => {
+        navigate('/MainPage');
+    };
 
   return (
     <div className='diagnosisAgreement-container'>
-      <Link to={`/MainPage`}>
+      <Link to={`/ProfileReadOnly`}>
         <div className='profile-icon'>
             <ImStarFull className="icon"/>
             <span className="text">PROFILE</span>
@@ -22,7 +31,20 @@ const DiagnosticTestAgreement = () => {
      </Link>
       <div className="diagnosisAgreement-modal">
         <div className="diagnosisAgreement-modal-content">
+            <span className="close" onClick={handleClose}>&times;</span>
             <h1>Privacy and Licanse Agreement</h1>
+            <div className="agreement-checkbox">
+                <input
+                type="checkbox"
+                id="agree"
+                checked={agree}
+                onChange={(e) => {
+                    setAgree(e.target.checked);
+                    setError(false); // Reset error state when checkbox changes
+                }}
+                />
+                <label htmlFor="agree" className={error ? 'text-error' : ''}>I Agree</label>
+            </div>
             <div className="save-button" onClick={handleSave}>
                 <IoIosCloudy className="icon-save" />
                 <span>OK</span>
--- dyslexia/src/Components/Main/Profile/ProfileReadOnly.js
+++ dyslexia/src/Components/Main/Profile/ProfileReadOnly.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import './Profile.css'
 import { ImStarFull } from "react-icons/im";
-import { useNavigate ,Link} from 'react-router-dom'
+import { useNavigate ,Link, useLocation} from 'react-router-dom'
 import { IoIosCloudy } from "react-icons/io";
 
-const Profile = () => {
+const ProfileReadOnly = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     const currentYear = new Date().getFullYear();
     const startYear = currentYear - 15;
     const endYear = currentYear - 4;
@@ -37,18 +38,8 @@ const Profile = () => {
         }
       }, []);
     
-      const handleChange = (event) => {
-        const { name, value } = event.target;
-        setProfile(prevProfile => ({
-          ...prevProfile,
-          [name]: value
-        }));
-      };
-    
-      const handleSave = () => {
-        localStorage.setItem('userInfo', JSON.stringify(profile));
-        alert('Profile updated successfully!');
-        // navigate('/someOtherPage');
+      const OK = () => {
+        navigate(-1);
       };
   return (
     <div className='profile-container'>
@@ -61,42 +52,36 @@ const Profile = () => {
       <div className="profile-modal">
         <div className="profile-modal-content">
             <h1>MY PROFILE</h1>
-            <input 
+          <input 
             type='text' 
             name='name' 
             value={profile.name}
-            onChange={handleChange}
+            readOnly
           />
           <input 
             type='text' 
             name='surname' 
             value={profile.surname}
-            onChange={handleChange}
+            readOnly
           />
-          <select
+          <input
+            type='text'
             name='gender'
             value={profile.gender}
-            onChange={handleChange}
-          >
-            <option value='' disabled hidden>Select Gender</option>
-            <option value='female'>Female</option>
-            <option value='male'>Male</option>
-          </select>
-          <select
-                    name='age'
-                    value={profile.age}
-                    onChange={handleChange}
-                    required
-                >
-                    <option value=''>Select Year of Birth</option>
-                    {ageOptions}
-                </select>
+            readOnly
+          />
+          <input
+            type='text'
+            name='age'
+            value={profile.age}
+            readOnly
+          />
           <input 
             type='text' 
             placeholder='Email'
             name='email'
             value={profile.email}
-            onChange={handleChange}
+            readOnly
           />
           <input 
             type='text' 
@@ -104,7 +89,7 @@ const Profile = () => {
             readOnly
             className='readonly-input'
           />
-          <div className="save-button" onClick={handleSave}>
+          <div className="save-button" onClick={OK}>
             <IoIosCloudy className="icon-save" />
             <span>OK</span>
           </div>
@@ -116,4 +101,4 @@ const Profile = () => {
   )
 }
 
-export default Profile
+export default ProfileReadOnly
