5e83d846622e3c359499aff38b5ad1d097dc938e
pinarrcindemirr <pnrcndmr01@gmail.com>
2024-02-26 23:13:10 +0300

    Update Test Information Page

--- dyslexia/src/Components/Main/DiagnosticTest/TestsInformation.js
+++ dyslexia/src/Components/Main/DiagnosticTest/TestsInformation.js
@@ -1,13 +1,39 @@
 import React, { useState, useEffect } from 'react'
-import './DiagnosticTestAgreement.css'
+import './TestsInformation.css'
 import { ImStarFull } from "react-icons/im";
 import { useNavigate ,Link} from 'react-router-dom'
 import { IoIosCloudy } from "react-icons/io";
 
 const TestsInformation = () => {
+  const navigate = useNavigate();
+
+  const handleStart= () => {
+    navigate('/MainPage');
+  };
+
+  const handleClose = () => {
+    navigate('/MainPage');
+  };
   return (
-    <div>
-      Test Info
+    <div className='testInfo-container'>
+      <Link to={`/ProfileReadOnly`}>
+        <div className='profile-icon'>
+            <ImStarFull className="icon"/>
+            <span className="text">PROFILE</span>
+        </div>
+     </Link>
+      <div className="testInfo-modal">
+        <div className="testInfo-modal-content">
+            <span className="close" onClick={handleClose}>&times;</span>
+            <h2>How to play the Test</h2>
+            <div className="start-button" onClick={handleStart}>
+                <IoIosCloudy className="start-icon-save" />
+                <span>Start Test </span>
+            </div>
+            <h1 className="testInfo-title">Test Name</h1>
+        </div>
+      </div>
+
     </div>
   )
 }
