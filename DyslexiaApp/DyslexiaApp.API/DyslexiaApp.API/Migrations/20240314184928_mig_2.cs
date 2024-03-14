3277678b4c297dcb34b90f2913f52ea26051d421
beyza çay <beyzacay@staj.com.tr>
2024-03-14 22:24:43 +0300

    Database connected. Token Jwt created. Password hashed service created

---
+++ DyslexiaApp/DyslexiaApp.API/DyslexiaApp.API/Migrations/20240314184928_mig_2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DyslexiaApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+        }
+    }
+}
