6b9e812e8cc4ee7bc1ed18cb1aaeb20da0fdaba8
beyza çay <beyzacay@staj.com.tr>
2024-03-14 22:42:33 +0300

    Revert "Database connected. Token Jwt created. Password hashed service created"
    This reverts commit 3277678b4c297dcb34b90f2913f52ea26051d421.

--- DyslexiaApp/Dyslexia.Core/Dyslexia.Core/Platforms/Android/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Dyslexia.Core
-{
-    // All the code in this file is only included on Android.
-    public class PlatformClass1
-    {
-    }
-}
--- DyslexiaApp/Dyslexia.Core/Dyslexia.Core/Platforms/iOS/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Dyslexia.Core
-{
-    // All the code in this file is only included on iOS.
-    public class PlatformClass1
-    {
-    }
-}
