a8870ff827ecfdd56ca175fb672a7b549bc6e11f
beyza çay <beyzacay@staj.com.tr>
2024-03-15 20:31:15 +0300

    as

--- DyslexiaAppMAUI.Shared/DyslexiaAppMAUI.Shared/obj/Debug/net8.0/DyslexiaAppMAUI.Shared.AssemblyInfo.cs
+++ DyslexiaAppMAUI.Shared/DyslexiaAppMAUI.Shared/obj/Debug/net8.0/DyslexiaAppMAUI.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DyslexiaAppMAUI.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9e812e8cc4ee7bc1ed18cb1aaeb20da0fdaba8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a5969d037568606e456aeb194cbacb570ad5a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DyslexiaAppMAUI.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DyslexiaAppMAUI.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
