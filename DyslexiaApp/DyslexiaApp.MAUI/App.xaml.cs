c214234cac314c47acf1e3c4d9546387770377a3
beyza çay <byzcay@gmail.com>
2024-03-17 16:18:44 +0300

    Reset Git cache and re-added files

---
+++ DyslexiaApp/DyslexiaApp.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DyslexiaApp.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DyslexiaApp/DyslexiaApp.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DyslexiaApp.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
