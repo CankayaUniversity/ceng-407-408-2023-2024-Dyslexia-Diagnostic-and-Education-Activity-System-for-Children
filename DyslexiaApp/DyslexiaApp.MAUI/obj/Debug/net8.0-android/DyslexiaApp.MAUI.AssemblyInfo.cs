3917da0f5816beed1e5c11bf40776becb258e5b4
Mert Aydın <mmertaydin4@gmail.com>
2024-03-18 20:31:16 +0300

    Updated

--- DyslexiaApp/DyslexiaApp.MAUI/obj/Debug/net8.0-android/DyslexiaApp.MAUI.AssemblyInfo.cs
+++ DyslexiaApp/DyslexiaApp.MAUI/obj/Debug/net8.0-android/DyslexiaApp.MAUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DyslexiaApp.MAUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4a70619ed621a57f94d04a3d6d39ab4b626dbe37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b26f2fa0813185e2dbfe1b3ee0f2576e9414ee11")]
 [assembly: System.Reflection.AssemblyProductAttribute("DyslexiaApp.MAUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DyslexiaApp.MAUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
