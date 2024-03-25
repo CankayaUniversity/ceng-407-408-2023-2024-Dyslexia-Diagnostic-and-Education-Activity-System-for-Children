067af0524bbcbf66e84c18a97d52e5b1d4ec5628
Mert Aydın <mmertaydin4@gmail.com>
2024-03-25 21:51:13 +0300

    Update

---
+++ DyslexiaApp/DyslexiaAppMAUI.Shared/Dtos/DyslexiaDiagnosisDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DyslexiaAppMAUI.Shared.Dtos;
+
+public record struct DyslexiaDiagnosisDto(
+    Guid Id,
+    int TestResults,
+    string FeedBack,
+    string Description,
+    MatchingGameDto[] MatchingGames,
+    NavigationGameDto[] NavigationGames
+);
+
+
+
---
+++ DyslexiaApp/DyslexiaAppMAUI.Shared/Dtos/SupportDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DyslexiaAppMAUI.Shared.Dtos;
+
+public record SupportDto(
+    Guid Id,
+    TimeSpan TimeSpent,
+    string FAQs,
+    string ContactString,
+    string Message,
+    string SupportStatus,
+    SistemDto Sistem
+);
+
+
+
