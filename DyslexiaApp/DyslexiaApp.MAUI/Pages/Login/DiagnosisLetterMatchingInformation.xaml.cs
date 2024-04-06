d9298f995eb6ac79e3ab280cb02cf776ad505878
pinarrcindemirr <pnrcndmr01@gmail.com>
2024-04-06 14:47:20 +0300

    Game/Test Page Design

---
+++ DyslexiaApp/DyslexiaApp.MAUI/Pages/Login/DiagnosisLetterMatchingInformation.xaml.cs
@@ -0,0 +1,13 @@
+namespace DyslexiaApp.MAUI.Pages.Login;
+
+public partial class DiagnosisLetterMatchingInformation : ContentPage
+{
+	public DiagnosisLetterMatchingInformation()
+	{
+		InitializeComponent();
+	}
+    private async void Close_Button(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+    }
+}
