06ae6982076fcd96e67fa7d35ab6313449b136de
pinarrcindemirr <pnrcndmr01@gmail.com>
2024-04-15 17:56:56 +0300

    Update MAUI

--- DyslexiaApp/DyslexiaAppMAUI.Shared/obj/Debug/net8.0/DyslexiaAppMAUI.Shared.AssemblyInfo.cs
+++ DyslexiaApp/DyslexiaAppMAUI.Shared/obj/Debug/net8.0/DyslexiaAppMAUI.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DyslexiaAppMAUI.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+278c447398a51aab07f994ab815746e531632df5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2089eae71544ca3b4928544eacc8b8276d9f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("DyslexiaAppMAUI.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DyslexiaAppMAUI.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
