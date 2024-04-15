6b2089eae71544ca3b4928544eacc8b8276d9f73
pinarrcindemirr <pnrcndmr01@gmail.com>
2024-04-15 17:37:30 +0300

    Revert "Game/Test Page Design"
    This reverts commit d9298f995eb6ac79e3ab280cb02cf776ad505878.

--- DyslexiaApp/DyslexiaApp.MAUI/Pages/Login/HomePage.xaml.cs
+++ DyslexiaApp/DyslexiaApp.MAUI/Pages/Login/HomePage.xaml.cs
@@ -6,17 +6,4 @@ public partial class HomePage : ContentPage
 	{
 		InitializeComponent();
 	}
-    private async void DiagnosisTapped(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new DiagnosticTestAgreement());
-    }
-    private async void EducationalTapped(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new EducationalGameList());
-    }
-
-    private async void OnProfileTapped(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new ProfilePage());
-    }
 }
--- DyslexiaApp/DyslexiaApp.MAUI/Pages/Login/RegisterAgreement.xaml.cs
+++ DyslexiaApp/DyslexiaApp.MAUI/Pages/Login/RegisterAgreement.xaml.cs
@@ -1,14 +1,11 @@
 namespace DyslexiaApp.MAUI.Pages.Login;
-using DyslexiaApp.MAUI.ViewModels;
-using DyslexiaAppMAUI.Shared.Dtos;
 
 public partial class RegisterAgreement : ContentPage
 {
-    public RegisterAgreement(AuthViewModel authViewModel)
-    {
-        InitializeComponent();
-        BindingContext= authViewModel;
-    }
+	public RegisterAgreement()
+	{
+		InitializeComponent();
+	}
 
     private async void OnCloseButtonClicked(object sender, EventArgs e)
     {
@@ -18,44 +15,12 @@ public partial class RegisterAgreement : ContentPage
     {
         if (AgreementCheckBox.IsChecked)
         {
-            if (BindingContext is AuthViewModel authViewModel)
-            {
-                IsBusy = true;
-                try
-                {
-                    var signupRequest = new SignupRequestDto(
-                        authViewModel.FirstName,
-                        authViewModel.LastName,
-                        authViewModel.Gender,
-                        authViewModel.Email,
-                        authViewModel.Password,
-                        authViewModel.Birthdate);
-
-                    var success = await authViewModel.TrySignupAsync(signupRequest);
-
-                    if (success)
-                    {
-                        await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
-                    }
-                    else
-                    {
-                        await DisplayAlert("Error", "Signup failed", "OK");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    await DisplayAlert("Error", ex.Message, "OK");
-                }
-                finally
-                {
-                    IsBusy = false;
-                }
-            }
+            await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
         }
         else
         {
             await DisplayAlert("Warning", "You must agree to the terms and conditions before registering.", "OK");
         }
-    }
 
+    }
 }
